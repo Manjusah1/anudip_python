ac3abb60822c816df461bb7a49957228065f8431
Manjusah1 <149500363+Manjusah1@users.noreply.github.com>
2023-11-02 22:53:22 +0530

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,15 @@
+x=10
+y=25
+if(x>y):
+    print(" x is the largest number")
+else:
+        print("y is the largest number")
+#area of the triangle
+        side1=10
+        side2=15
+        side3=20
+        s=(10+15+20)/2
+        print("semi perimeter of the triangle is :", s)
+        area=(s*(s-10)*(s-15)*(s-20))**0.5
+        print("area of the triangle is :", area)
+        
